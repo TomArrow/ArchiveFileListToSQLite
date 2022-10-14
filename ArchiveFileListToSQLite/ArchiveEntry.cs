a95ae30cc51adc83fe87e909ec1b2a848dc911ba
Tom <tomarrowtomarrow@hotmail.com>
2022-10-14 21:20:29 +0200

    Add project files.

---
+++ ArchiveFileListToSQLite/ArchiveEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveFileListToSQLite
+{
+    class ArchiveEntry
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public string Key { get; set; }
+        [Indexed]
+        public string FileExtension { get; set; }
+        [Indexed]
+        public long Size { get; set; } // for example "youtube"
+        [Indexed]
+        public long CompressedSize { get; set; } // for example "youtube"
+        [Indexed]
+        public DateTime? CreatedDate { get; set; }
+        [Indexed]
+        public DateTime? LastAccessedDate { get; set; }
+        [Indexed]
+        public DateTime? LastModifiedDate { get; set; }
+        [Indexed]
+        public bool IsDirectory { get; set; }
+    }
+}
---
+++ ArchiveFileListToSQLite/Program.cs
@@ -0,0 +1,72 @@
+﻿using SharpCompress.Archives.SevenZip;
+using SharpCompress.Readers;
+using SQLite;
+using System;
+using System.IO;
+
+namespace ArchiveFileListToSQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                processFile(arg);
+            }
+        }
+
+        static void processFile(string path)
+        {
+
+            using (FileStream fs = File.Open(path,FileMode.Open,FileAccess.Read,FileShare.Read))
+            {
+
+                using(SevenZipArchive archive = SevenZipArchive.Open(fs))
+                {
+
+                    using (var db = new SQLiteConnection(path + ".db", false))
+                    {
+
+                        db.CreateTable<ArchiveEntry>();
+
+                        db.BeginTransaction();
+                        foreach (SevenZipArchiveEntry entry in archive.Entries)
+                        {
+                            //Console.WriteLine(entry.ToString());
+
+                            // Key: full path to compressed file
+                            // IsDirectory: obvious
+                            // Size: obvious
+                            // LastAccessedTime
+                            // LastModifiedTime
+                            // CreatedTime
+                            // CompressedSize
+
+                            ArchiveEntry archiveEntry = new ArchiveEntry()
+                            {
+                                Key = entry.Key,
+                                FileExtension = Path.GetExtension(entry.Key).Trim('.'),
+                                IsDirectory = entry.IsDirectory,
+                                Size = entry.Size,
+                                CompressedSize = entry.CompressedSize,
+                                LastAccessedDate = entry.LastAccessedTime,
+                                LastModifiedDate = entry.LastModifiedTime,
+                                CreatedDate = entry.CreatedTime,
+                            };
+
+
+                            db.Insert(archiveEntry);
+                            //Console.WriteLine(entry.ToString());
+                        }
+
+
+                        db.Commit();
+                        db.Close();
+                    }
+                }
+
+            }
+        }
+    }
+}
